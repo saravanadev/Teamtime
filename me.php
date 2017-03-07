bcb89c102abfff9ef73e0a45fa820f4bfc1432e4
Robel kebede <robelkebede44@gmail.com>
2017-03-07 03:53:35 -0800

    the profile page

---
+++ me.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<link rel="stylesheet" href="boot/css/bootstrap.css" type="text/css" />
+</head>
+<body>
+<?php
+session_start();
+?>
+<span class="glyphicon glyphicon-search"></span>
+
+</body>
+</html>
